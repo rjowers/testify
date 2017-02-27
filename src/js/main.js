6e5da514a6d9e502a23712f971a69286eda963f8
Ryan Jowers <rjowers@g.clemson.edu>
2017-02-27 15:12:43 -0500

    all tests are passed

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,11 @@ GildedRose.prototype.tick = function () {
   if (this.name != 'Aged Brie' && this.name != 'Backstage passes to a TAFKAL80ETC concert') {
     if (this.quality > 0) {
       if (this.name != 'Sulfuras, Hand of Ragnaros') {
-        this.quality = this.quality - 1;
+        if(this.name == 'Conjured Mana Cake') {
+          this.quality = this.quality - 2;
+        } else {
+          this.quality = this.quality - 1;
+          }
       }
     }
   } else {
@@ -31,17 +35,18 @@ GildedRose.prototype.tick = function () {
   if (this.name != 'Sulfuras, Hand of Ragnaros') {
     this.sellIn = this.sellIn - 1;
   }
-
+////////Sell IN
   if (this.sellIn < 0) {
     if (this.name != 'Aged Brie') {
       if (this.name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.quality > 0) {
           if (this.name != 'Sulfuras, Hand of Ragnaros') {
-            this.quality = this.quality - 1;
+            if (this.name === 'Conjured Mana Cake'){
+               this.quality = this.quality - 2;
+             }else{
+               this.quality = this.quality - 1;
+             }
           }
-        }
-        if (this.name === 'Conjured Mana Cake') {
-          this.quality = this.quality -2;
         }
       } else {
         this.quality = this.quality - this.quality;
